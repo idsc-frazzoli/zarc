14632a41416118fb7c11c1df3f8cef7e507e8b1c
BrettRStephens <sbrett@student.ethz.ch>
2017-11-14 14:32:58 +0100

    updated pytalker node

--- workspace/src/node_example/src/pytalker.py
+++ workspace/src/node_example/src/pytalker.py
@@ -11,7 +11,7 @@ import rospy
 from dynamic_reconfigure.server import Server as DynamicReconfigureServer
 
 # Import custom message data and dynamic reconfigure variables.
-from node_example.msg import NodeExampleData
+from node_example.msg import ECU
 from node_example.cfg import nodeExampleConfig as ConfigType
 
 class NodeExample(object):
@@ -26,7 +26,7 @@ class NodeExample(object):
         # Create a dynamic reconfigure server.
         self.server = DynamicReconfigureServer(ConfigType, self.reconfigure_cb)
         # Create a publisher for our custom message.
-        self.pub = rospy.Publisher('example', NodeExampleData, queue_size=10)
+        self.pub = rospy.Publisher('example', ECU, queue_size=10)
         # Initialize message variables.
         self.enable = rospy.get_param('~enable', True)
         self.int_motor = rospy.get_param('~motor', 90)
@@ -43,19 +43,21 @@ class NodeExample(object):
         Called at a specified interval. Publishes message.
         """
         if not self.enable:
-            msg = NodeExampleData()
+            # reset msg to "90" when enable unselected
+            msg = ECU()
             msg.motor = 90
             msg.servo = 90
             self.pub.publish(msg)
             return
 
         # Set the message type to publish as our custom message.
-        msg = NodeExampleData()
+        msg = ECU()
         # Assign message fields to values from the parameter server.
         msg.motor = rospy.get_param('~motor', self.int_motor)
         msg.servo = rospy.get_param('~servo', self.int_servo)
 
-        # Fill in custom message variables with values updated from dynamic
+        # Fill in custom
+        # message variables with values updated from dynamic
         # reconfigure server.
         self.int_motor = msg.motor
         self.int_servo = msg.servo
@@ -69,9 +71,13 @@ class NodeExample(object):
         '''
         # Fill in local variables with values received from dynamic reconfigure
         # clients (typically the GUI).
+        self.enable = config["enable"]
+        # reset GUI to "90" when enable is unselected
+        if not self.enable:
+            config["motor"] = 90
+            config["servo"] = 90
         self.int_motor = config["motor"]
         self.int_servo = config["servo"]
-        self.enable = config["enable"]
         # Return the new variables.
         return config
 
