f4081afa7b8b62e66261f6a93c244369cec72948
jelavice <edo.jelavic@gmail.com>
2017-11-15 20:03:46 +0100

    Added logging of quaternions since Eigen gives discontinuous shit

--- workspace/src/data_logging/src/logging_node.cpp
+++ workspace/src/data_logging/src/logging_node.cpp
@@ -33,7 +33,7 @@ struct DataVicon {
     double time;
     Eigen::Matrix<double, 3, 1> pos;
     Eigen::Matrix<double, 3, 1> rot;
-
+    Eigen::Quaternion<double> q;
 };
 
 class ViconDataLogger {
@@ -55,8 +55,8 @@ public:
         DataVicon dataVicon;
         dataVicon.time = msg->header.stamp.sec + msg->header.stamp.nsec * 1e-9 - timeOffset;
         dataVicon.pos = Eigen::Matrix<double, 3, 1>(msg->transform.translation.x, msg->transform.translation.y, msg->transform.translation.z);
-        Eigen::Quaternion<double> q(msg->transform.rotation.w, msg->transform.rotation.x, msg->transform.rotation.y, msg->transform.rotation.z);
-        dataVicon.rot = q.toRotationMatrix().eulerAngles(0, 1, 2);
+        dataVicon.q = Eigen::Quaternion<double>(msg->transform.rotation.w, msg->transform.rotation.x, msg->transform.rotation.y, msg->transform.rotation.z);
+        dataVicon.rot = dataVicon.q.toRotationMatrix().eulerAngles(0, 1, 2);
 
         m_buffer.push_back(dataVicon);
     }
@@ -74,11 +74,12 @@ public:
         m_file.open(filename);
 
         // write the file headers
-        m_file << "t, x, y, z, angX, angY, angZ" << std::endl;
+        m_file << "t, x, y, z, angX, angY, angZ, qx, qy, qz, qw" << std::endl;
 
         for (boost::circular_buffer<DataVicon>::const_iterator it = m_buffer.begin(); it != m_buffer.end(); it++)
-            m_file << it->time << ", " << it->pos(0) << ", " << it->pos(1) << ", " << it->pos(2) << ", " << it->rot(0) << ", " << it->rot(1) << ", "
-                    << it->rot(2) << std::endl;
+            m_file << it->time << ", " << it->pos(0) << ", " << it->pos(1) << ", " << it->pos(2) << ", "
+            << it->rot(0) << ", " << it->rot(1) << ", " << it->rot(2) <<", "
+            << it->q.x() << ", " << it->q.y() << ", " << it->q.z() << ", " << it->q.w() << std::endl;
 
         // close the output file
         m_file.close();
