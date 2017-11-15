9c37b18b88c42b3488fe3f29b6ebe3a2a41ecb70
jelavice <edo.jelavic@gmail.com>
2017-11-15 17:22:05 +0100

    Dumping upon ctrl + c

--- workspace/src/data_logging/src/logging_node.cpp
+++ workspace/src/data_logging/src/logging_node.cpp
@@ -5,8 +5,6 @@
  *      Author: jelavice
  */
 
-
-
 // inlcude iostream and string libraries
 #include <iostream>
 #include <fstream>
@@ -14,12 +12,16 @@
 #include <boost/circular_buffer.hpp>
 #include <geometry_msgs/TransformStamped.h>
 #include <Eigen/Dense>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
 
+volatile sig_atomic_t flag = 0;
 
+void signalCallback(int sig) { // can be called asynchronously
+    flag = 1; // set flag
+}
 
-// create an ofstream for the file output (see the link on streams for
-// more info)
-std::ofstream file;
 // create a name for the file output
 std::string filename = "exampleOutput.csv";
 
@@ -29,93 +31,107 @@ int A;
 int B;
 int C;
 
-
 struct DataVicon {
 
     double time;
-    Eigen::Matrix<double,3,1> pos;
+    Eigen::Matrix<double, 3, 1> pos;
     Eigen::Quaternion<double> q;
 };
 
 class ViconDataLogger {
 
-
-    ViconDataLogger(int buffSize) {
+public:
+    ViconDataLogger(int buffSize, std::string filename) :
+            m_filename(filename) {
         m_buffer.set_capacity(buffSize);
     }
 
-    void msgCallback(geometry_msgs::TransformStamped::ConstPtr msg){
+    void msgCallback(geometry_msgs::TransformStamped::ConstPtr msg) {
 
         DataVicon dataVicon;
-        dataVicon.time = msg->header.stamp.sec + msg->header.stamp.nsec*1e-9;
-        dataVicon.pos = Eigen::Matrix<double,3,1>(msg->transform.translation.x, msg->transform.translation.y, msg->transform.translation.z);
-        dataVicon.q = Eigen::Quaternion<double> (msg->transform.rotation.x, msg->transform.rotation.y, msg->transform.rotation.z, msg->transform.rotation.w);
+        dataVicon.time = msg->header.stamp.sec + msg->header.stamp.nsec * 1e-9;
+        dataVicon.pos = Eigen::Matrix<double, 3, 1>(msg->transform.translation.x, msg->transform.translation.y, msg->transform.translation.z);
+        dataVicon.q = Eigen::Quaternion<double>(msg->transform.rotation.x, msg->transform.rotation.y, msg->transform.rotation.z, msg->transform.rotation.w);
         m_buffer.push_back(dataVicon);
 
     }
 
+    void run() {
+
+        if (flag)
+            dumpToFile();
+    }
+
+    void dumpToFile() {
+
+        //TODO dump the actual files here
+        // create and open the .csv file
+        m_file.open(filename);
+
+        // write the file headers
+        m_file << "Column A" << "," << "Column B" << "Column C" << std::endl;
+
+        // i is just a variable to make numbers with in the file
+        i = 1;
+
+        // write data to the file
+        for (int counter = 0; counter < 10; counter++) {
+            // make some data
+            A = i + 5;
+            B = i + 10;
+            C = i + 20;
+
+            // write the data to the output file
+            m_file << A << "," << B << "," << C << std::endl;
+
+            // increase i
+            i = i * 5;
+        }
+
+        // close the output file
+        m_file.close();
+    }
+
 private:
     boost::circular_buffer<DataVicon> m_buffer;
+    std::string m_filename;
+    std::ofstream m_file;
 
 };
 
 struct Data {
-    Data (int a, int b){first  = a; second = b;}
+    Data(int a, int b) {
+        first = a;
+        second = b;
+    }
     int first;
     int second;
 };
-void printBuffer (const boost::circular_buffer<Data>& buffer) {
+
+void printBuffer(const boost::circular_buffer<Data>& buffer) {
     for (boost::circular_buffer<Data>::const_iterator it = buffer.begin(); it != buffer.end(); it++)
         std::cout << "(" << it->first << ", " << it->second << ")" << " ";
     std::cout << std::endl << std::endl;
 }
 
+int main(int argc, char** argv) {
 
-
-
-
-
-int main(int argc, char** argv)
-{
-
-
-
+    signal(SIGINT, signalCallback);
 
     boost::circular_buffer<Data> buffer(5);
+    ViconDataLogger viconDataLogger(5, filename);
 
-
-    for (int i=0, j=9; i < 10; i++, j--){
-        buffer.push_back(Data(i,j));
-        printBuffer(buffer);
+    for (int i = 0, j = 9; i < 10; i++, j--) {
+        buffer.push_back(Data(i, j));
     }
 
-
-
-    // create and open the .csv file
-    file.open(filename);
-
-    // write the file headers
-    file << "Column A" << "," << "Column B" << "Column C" << std::endl;
-
-    // i is just a variable to make numbers with in the file
-    i = 1;
-
-    // write data to the file
-    for (int counter = 0; counter <  10; counter++)
-    {
-        // make some data
-        A = i + 5;
-        B = i + 10;
-        C = i + 20;
-
-        // write the data to the output file
-        file << A << "," << B << "," << C << std::endl;
-
-        // increase i
-        i = i * 5;
+    while (true) {
+        viconDataLogger.run();
+        if (flag) {
+            std::cout << " \n Data logger node terminated. Saving measurements into: " << filename << std::endl;
+            break;
+        }
     }
 
-    // close the output file
-    file.close();
     return 0;
 }
