a1bba8c90a1874dcd7ecf89c7408c71efcaa136d
jelavice <edo.jelavic@gmail.com>
2017-11-17 18:09:21 +0100

    Separated logging nodes

--- workspace/src/data_logging/src/odroid_logging_node.cpp
+++ workspace/src/data_logging/src/odroid_logging_node.cpp
@@ -88,24 +88,6 @@ private:
 
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 struct ImuData {
 
     double time;
@@ -160,9 +142,8 @@ public:
         m_file << "t, a_x, a_y, a_z, w_x, w_y, w_z, qx, qy, qz, qw " << std::endl;
 
         for (boost::circular_buffer<ImuData>::const_iterator it = m_buffer.begin(); it != m_buffer.end(); it++)
-            m_file << it->time << ", " << it->linAcc(0) << ", " << it->linAcc(1) << ", " << it->linAcc(2) << ", "
-            << it->angVel(0) << ", " << it->angVel(1) << ", " << it->angVel(2) << ", "
-            << it->q.x() << ", " << it->q.y() << ", " << it->q.z() << ", " << it->q.w() << std::endl;
+            m_file << it->time << ", " << it->linAcc(0) << ", " << it->linAcc(1) << ", " << it->linAcc(2) << ", " << it->angVel(0) << ", " << it->angVel(1)
+                    << ", " << it->angVel(2) << ", " << it->q.x() << ", " << it->q.y() << ", " << it->q.z() << ", " << it->q.w() << std::endl;
 
         // close the output file
         m_file.close();
@@ -175,103 +156,16 @@ private:
 
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-struct DataVicon {
-
-    double time;
-    Eigen::Matrix<double, 3, 1> pos;
-    Eigen::Matrix<double, 3, 1> rot;
-    Eigen::Quaternion<double> q;
-};
-
-class ViconDataLogger {
-
-public:
-    ViconDataLogger(int buffSize, std::string filename) :
-            m_filename("Vicon_" + filename) {
-        m_buffer.set_capacity(buffSize);
-
-    }
-
-    void msgCallback(geometry_msgs::TransformStamped::ConstPtr msg) {
-
-        static double timeOffset = -1;
-
-        if (timeOffset < 0)
-            timeOffset = msg->header.stamp.sec + msg->header.stamp.nsec * 1e-9;
-
-        DataVicon dataVicon;
-        dataVicon.time = msg->header.stamp.sec + msg->header.stamp.nsec * 1e-9 - timeOffset;
-        dataVicon.pos = Eigen::Matrix<double, 3, 1>(msg->transform.translation.x, msg->transform.translation.y, msg->transform.translation.z);
-        dataVicon.q = Eigen::Quaternion<double>(msg->transform.rotation.w, msg->transform.rotation.x, msg->transform.rotation.y, msg->transform.rotation.z);
-        dataVicon.rot = dataVicon.q.toRotationMatrix().eulerAngles(0, 1, 2);
-
-        m_buffer.push_back(dataVicon);
-    }
-
-    void run() {
-
-    }
-
-    void dumpToFile() {
-
-        // create and open the .csv file
-        m_file.open(filename);
-
-        // write the file headers
-        m_file << "t, x, y, z, angX, angY, angZ, qx, qy, qz, qw" << std::endl;
-
-        for (boost::circular_buffer<DataVicon>::const_iterator it = m_buffer.begin(); it != m_buffer.end(); it++)
-            m_file << it->time << ", " << it->pos(0) << ", " << it->pos(1) << ", " << it->pos(2) << ", " << it->rot(0) << ", " << it->rot(1) << ", "
-                    << it->rot(2) << ", " << it->q.x() << ", " << it->q.y() << ", " << it->q.z() << ", " << it->q.w() << std::endl;
-
-        // close the output file
-        m_file.close();
-    }
-
-private:
-    boost::circular_buffer<DataVicon> m_buffer;
-    std::string m_filename;
-    std::ofstream m_file;
-
-};
-
 int main(int argc, char** argv) {
 
     int buffSize = 10000;
-    ViconDataLogger viconDataLogger(buffSize, filename);
     ImuDataLogger imuDataLogger(buffSize, filename);
     VelDataLogger velDataLogger(buffSize, filename);
-    ros::init(argc, argv, "data_logger");
+    ros::init(argc, argv, "odroid_data_logger");
     ros::NodeHandle n;
-    ros::Subscriber sub, sub2, sub3;
-    sub = n.subscribe("vicon/CAR/CAR", 1, &ViconDataLogger::msgCallback, &viconDataLogger);
-    sub2 = n.subscribe("imu/Data", 1, &ImuDataLogger::msgCallback, &imuDataLogger);
-    sub3 = n.subscribe("forward_vel", 1, &VelDataLogger::msgCallback, &velDataLogger);
+    ros::Subscriber sub, sub2;
+    sub = n.subscribe("imu/Data", 1, &ImuDataLogger::msgCallback, &imuDataLogger);
+    sub2 = n.subscribe("forward_vel", 1, &VelDataLogger::msgCallback, &velDataLogger);
 
     signal(SIGINT, signalCallback);
 
@@ -279,12 +173,10 @@ int main(int argc, char** argv) {
         ros::spinOnce();
         if (flag) {
             std::cout << " \n Data logger node terminated. Saving measurements into: " << filename << std::endl;
-            viconDataLogger.dumpToFile();
             imuDataLogger.dumpToFile();
             velDataLogger.dumpToFile();
             break;
         }
-        ros::Duration(0.01).sleep();
     }
     ros::shutdown();
     return 0;
