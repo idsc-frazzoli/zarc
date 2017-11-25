3a486bc3dc6567cb5be65ad604b52960836c1338
jelavice <edo.jelavic@gmail.com>
2017-11-25 17:03:14 +0100

    VIcon logger should be fine

--- workspace/src/data_logging/include/logger.h
+++ workspace/src/data_logging/include/logger.h
@@ -23,41 +23,29 @@ public:
     typedef boost::circular_buffer<std::vector<double> > buffer_t;
 
     DataLogger(int buffSize, std::string filename, int queueSize, std::string header) :
-        m_filename(filename), m_header(header) {
+            m_filename(filename), m_header(header), m_dumpedToFile(false) {
         m_buffer.set_capacity(buffSize);
     }
 
-    virtual ~DataLogger() {};
+    virtual ~DataLogger() {
+        dumpToFile();
+    }
 
     virtual void msgCallback(MSGTYPEPTR msg) = 0;
 
-    std::string getCurrTime(){
-        time_t rawtime;
-        struct tm * timeinfo;
-        char buffer[80];
+    void dumpToFile() {
 
-        time (&rawtime);
-        timeinfo = localtime(&rawtime);
-
-        strftime(buffer,sizeof(buffer),"%d-%m-%Y %I:%M:%S",timeinfo);
-        return std::string(buffer);
-
-
-    }
-
-    void dumpToFile () {
-
+        if(m_dumpedToFile)
+            return;
 
         std::string fileName = m_filename + "_" + getCurrTime() + ".csv";
-
         std::ofstream file;
-
         file.open(fileName);
 
         // write the file headers
         file << m_header << std::endl;
 
-        for (buffer_t::const_iterator it = m_buffer.begin(); it != m_buffer.end(); it++){
+        for (buffer_t::const_iterator it = m_buffer.begin(); it != m_buffer.end(); it++) {
             for (int i = 0; i < it->size(); i++)
                 file << it->at(i) << ", ";
             file << std::endl;
@@ -65,21 +53,33 @@ public:
 
         // close the output file
         file.close();
+
+        m_dumpedToFile = true;
+    }
+protected:
+
+    std::string getCurrTime() {
+        time_t rawtime;
+        struct tm * timeinfo;
+        char buffer[80];
+
+        time(&rawtime);
+        timeinfo = localtime(&rawtime);
+
+        strftime(buffer, sizeof(buffer), "%d-%m-%Y %I:%M:%S", timeinfo);
+        return std::string(buffer);
+
     }
 
-
-    void addToBuff(const std::vector<double>& vec){
-
+    void addToBuff(const std::vector<double>& vec) {
+        m_buffer.push_back(vec);
     }
-
-
-
 
 private:
     buffer_t m_buffer;
     std::string m_filename;
     std::string m_header;
-
+    bool m_dumpedToFile;
 
 };
 
--- workspace/src/data_logging/src/vicon_data_logger.cpp
+++ workspace/src/data_logging/src/vicon_data_logger.cpp
@@ -7,30 +7,40 @@
 
 #include "vicon_data_logger.h"
 
-
 //TODO do this in the base class
-ViconDataLogger::ViconDataLogger(int buffSize, std::string filename, std::string topic, ros::NodeHandle& n, int queueSize, std::string header):
-ViconDataLogger::BASE(buffSize,filename, queueSize, header)
-{
-
+ViconDataLogger::ViconDataLogger(int buffSize, std::string filename, std::string topic, ros::NodeHandle& n, int queueSize, std::string header) :
+        BASE(buffSize, filename, queueSize, header) {
     m_sub = n.subscribe(topic, queueSize, &ViconDataLogger::msgCallback, this);
 }
 
-void ViconDataLogger::msgCallback(geometry_msgs::TransformStamped::ConstPtr msg) {
+void ViconDataLogger::msgCallback(msgPtr_t msg) {
 
     static double timeOffset = -1;
 
     if (timeOffset < 0)
         timeOffset = msg->header.stamp.sec + msg->header.stamp.nsec * 1e-9;
-    //TODO move this to a different function;
-    DataVicon dataVicon;
-    dataVicon.time = msg->header.stamp.sec + msg->header.stamp.nsec * 1e-9 - timeOffset;
-    dataVicon.pos = Eigen::Matrix<double, 3, 1>(msg->transform.translation.x, msg->transform.translation.y, msg->transform.translation.z);
-    dataVicon.q = Eigen::Quaternion<double>(msg->transform.rotation.w, msg->transform.rotation.x, msg->transform.rotation.y, msg->transform.rotation.z);
-    dataVicon.rot = dataVicon.q.toRotationMatrix().eulerAngles(0, 1, 2);
 
-    //TODO push the data in the buffer
+    //magic number, number of elements that we want to insert
+    int numElem = 11;
+    std::vector<double> data(numElem);
+
+    //the order has to match to the one in the header
+    data.push_back(msg->header.stamp.sec + msg->header.stamp.nsec * 1e-9 - timeOffset);
+    data.push_back(msg->transform.translation.x);
+    data.push_back(msg->transform.translation.y);
+    data.push_back(msg->transform.translation.z);
+    data.push_back(msg->transform.rotation.w);
+    data.push_back(msg->transform.rotation.x);
+    data.push_back(msg->transform.rotation.y);
+    data.push_back(msg->transform.rotation.z);
+
+    Eigen::Quaternion<double> q(msg->transform.rotation.w, msg->transform.rotation.x, msg->transform.rotation.y, msg->transform.rotation.z);
+    Eigen::Matrix<double, 3, 1> rot = q.toRotationMatrix().eulerAngles(0, 1, 2);
+    data.push_back(rot(0));
+    data.push_back(rot(1));
+    data.push_back(rot(2));
+
+    addToBuff(data);
 
 }
-
 
