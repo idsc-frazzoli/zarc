b12e5b616c9dbb74a8863e59a1c636f0adfac758
jelavice <edo.jelavic@gmail.com>
2017-11-25 20:43:31 +0100

    Adding yaml

--- workspace/src/data_logging/src/logger_node.cpp
+++ workspace/src/data_logging/src/logger_node.cpp
@@ -5,12 +5,7 @@
  *      Author: jelavice
  */
 
-/*
- * logging_node.cpp
- *
- *  Created on: Nov 15, 2017
- *      Author: jelavice
- */
+
 
 // inlcude iostream and string libraries
 #include <iostream>
@@ -20,6 +15,7 @@
 #include <unistd.h>
 #include <ros/ros.h>
 #include <boost/filesystem.hpp>
+#include <>yaml-cpp/yaml.h>
 
 #include "encoder_data_logger.h"
 #include "imu_data_logger.h"
@@ -32,29 +28,51 @@ void signalCallback(int sig) {
     flag = 1;
 }
 
-//don't need this unless I want to do some dynamic loading
-//class LoggerFactory {
-//
-//public:
-//
-//    enum LoggerType {
-//        VICON,
-//        IMU,
-//        ENCODER
-//    };
-//
-//};
+class LoggerFactory {
+
+public:
+
+    enum LoggerType {
+        VICON,
+        IMU,
+        ENCODER
+    };
+
+LoggerFactory(){}
+~LoggerFactory(){}
+
+void loadSettings(std::string filename){
+    YAML::Node baseNode = YAML::Load(filename);
+    if (baseNode.IsNull()) return false; //File Not Found?
+}
+
+
+
+
+};
 
 int main(int argc, char** argv) {
+
+
+    if (argc < 2)
+        throw std::runtime_error("No config file provided");
+
+    std::string configFile(argv[1]);
+
+    std::cout << configFile << std::endl;
+
+    LoggerFactory loggerFactory;
+    loggerFactory.loadSettings(configFile);
+
 
     int buffSize = 10000;
     ros::init(argc, argv, "data_logger");
     ros::NodeHandle n;
     double sleepTime = 0.01;
 
-    vicon_log::ViconDataLogger viconDataLogger(buffSize, "viconOutput", "vicon/CAR/CAR", n, 1, "t, x, y, z, angX, angY, angZ, qx, qy, qz, qw");
-    imu_log::ImuDataLogger imuDataLogger(buffSize, "imuOutput", "imu/data", n, 1000, "t, a_x, a_y, a_z, w_x, w_y, w_z, qx, qy, qz, qw");
-    enc_log::EncDataLogger encDataLogger(buffSize, "encOutput", "forward_vel", n, 1000, "time, velFL, velFR, velBL, velBR");
+//    vicon_log::ViconDataLogger viconDataLogger(buffSize, "viconOutput", "vicon/CAR/CAR", n, 1, "t, x, y, z, angX, angY, angZ, qx, qy, qz, qw");
+//    imu_log::ImuDataLogger imuDataLogger(buffSize, "imuOutput", "imu/data", n, 1000, "t, a_x, a_y, a_z, w_x, w_y, w_z, qx, qy, qz, qw");
+//    enc_log::EncDataLogger encDataLogger(buffSize, "encOutput", "forward_vel", n, 1000, "time, velFL, velFR, velBL, velBR");
 
     //current working directory
     boost::filesystem::path full_path(boost::filesystem::current_path());
